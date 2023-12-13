d16cb97acbd3472659c7752c36b7d1b20a6b0b4b
TedCreator <takahori2@gmail.com>
2023-12-13 11:52:10 -0500

    Wrote timeToSec function

--- shutdown.py
+++ shutdown.py
@@ -3,10 +3,24 @@ from PyQt6.QtWidgets import QWidget, QApplication
 from QtDesignWindow import Ui_MainWindow
 import sys
 
+
+
+
 class Window(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj = None, **kwargs):
         super(Window, self,).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.scheduleButton.clicked.connect(self.timeToSec)
+
+    def timeToSec(self):
+        # 1hr:3600s   |   1min:60s    |    1s:1s
+        totalSec = (self.hoursInput.value() * 3600) + (self.minutesInput.value() * 60) + self.secondsInput.value()
+        print(totalSec)
+        return totalSec
+    
+
+    
+    
         
 def main():
     app = QtWidgets.QApplication(sys.argv)
