12f0d9c07fc53d71537a0072316d44a88b275846
TedCreator <takahori2@gmail.com>
2023-12-16 04:20:39 -0500

    Added shutdown command

--- shutdown.py
+++ shutdown.py
@@ -7,72 +7,60 @@ import sys
 import os
 
 class Countdown(QtWidgets.QWidget, Ui_Dialog):
+    # Method has to be above _init_ so it exists before object is destroyed
+    def stopTimer(self):
+        self.timer.stop()
+        self.lcdHours.display(0)
+        self.lcdMinutes.display(0)
+        self.lcdSeconds.display(0)
+
     def __init__(self, *args, obj = None, **kwargs):
         super(Countdown, self,).__init__(*args, **kwargs)
         self.setupUi(self)
-        
+        self.setStyleSheet(getStylesFromPath("CSS/CountdownWindow.css"))
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.updateCountdown)
+        self.destroyed.connect(self.stopTimer)
+        self.remainingtime = -1
+
+    def startTimer(self, time):
+        self.remainingtime = time
+        self.show()
+        self.updateCountdown()
+        self.timer.start(1000) # countdown every 1000ms
+
+    def updateCountdown(self):
+        if self.remainingtime > 0:
+            hours, r = divmod(self.remainingtime, 3600)
+            minutes, seconds = divmod(r, 60)
+            self.lcdHours.display(hours)
+            self.lcdMinutes.display(minutes)
+            self.lcdSeconds.display(seconds)
+            self.remainingtime -= 1
+        elif self.remainingtime == 0: 
+            self.stopTimer()
+            shutdown()
+            self.destroy()
 
 class Window(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj = None, **kwargs):
         super(Window, self,).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.setStyleSheet(getStylesFromPath("CSS/MainWindow.css"))
         self.removeSpinButtons()
         self.scheduleButton.clicked.connect(self.scheduleButtonClicked)
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.updateCountdown)
-        self.remainingtime = -1
         self.countdown = Countdown()
-        
+
     def removeSpinButtons(self):
         self.hoursInput.setButtonSymbols(QSpinBox.ButtonSymbols.NoButtons)
         self.minutesInput.setButtonSymbols(QSpinBox.ButtonSymbols.NoButtons)
         self.secondsInput.setButtonSymbols(QSpinBox.ButtonSymbols.NoButtons)
 
     def scheduleButtonClicked(self):
-        if self.scheduleButton.text() == "Schedule":
-            self.startCountdown()
-            if self.remainingtime > 0:
-                self.scheduleButton.setText("Reset")
-        else:
-            self.resetCountdown()
-            self.scheduleButton.setText("Schedule")
-
-    def startCountdown(self):
-        self.remainingtime = timeToSec(self.hoursInput.value(), self.minutesInput.value(), self.secondsInput.value())
-        self.countdown.setStyleSheet(getStylesFromPath("CSS/CountdownWindow.css"))
-        self.countdown.show()
-        self.updateCountdown()
-        self.timer.start(1000)
-
-    def resetCountdown(self):
-        self.timer.stop()
-        self.countdown.lcdHours.display(0)
-        self.countdown.lcdMinutes.display(0)
-        self.countdown.lcdSeconds.display(0)
-
-    def updateCountdown(self):
-        if self.remainingtime > 0:
-            hours, r = divmod(self.remainingtime, 3600)
-            minutes, seconds = divmod(r, 60)
-            self.countdown.lcdHours.display(hours)
-            self.countdown.lcdMinutes.display(minutes)
-            self.countdown.lcdSeconds.display(seconds)
-            self.remainingtime -= 1
-        elif self.remainingtime == 0: 
-            self.countdown.lcdSeconds.display(0) # cleans up the display
-            self.timer.stop()
-            self.countdown.hide()
-            self.scheduleButton.setText("Schedule")
-            self.shutdown()
+        self.startCountdown()
         
-    def generateShutdownCommand(self):
-        if sys.platform == "win32":
-            return "shutdown -s -t " + str(0)
-        else:
-            return "shutdown -p"
-
-    def shutdown(self):
-        os.system(self.generateShutdownCommand())
+    def startCountdown(self):
+        self.countdown.startTimer(timeToSec(self.hoursInput.value(), self.minutesInput.value(), self.secondsInput.value()))
 
 def getStylesFromPath(path):
     stylesheet = QFile(path)
@@ -91,11 +79,20 @@ def timeToSec(h, m, s):
     totalSeconds = ((h * 3600) + (m * 60) + s)
     return totalSeconds
 
+def generateShutdownCommand():
+    if sys.platform == "win32":
+        return "shutdown -s -t " + str(0)
+    else:
+        return "shutdown -p"
+
+def shutdown():
+    os.system(generateShutdownCommand())
+
 def main():
     # UI
     app = QtWidgets.QApplication(sys.argv)
     window = Window()
-    window.setStyleSheet(getStylesFromPath("CSS/MainWindow.css"))
+    
     window.show()
     app.exec()
 
